33ab0fb41ee63957d0c4fb7a6f9d0abe7b2ccae4
Erik Hommel <hommel@ee-atwork.nl>
2015-04-21 11:15:08 +0200

    report for issue 1979 activities with drupal roles

---
+++ CRM/Civireports/Form/Report/RoleActivities.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Civireports_Form_Report_RoleActivities',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'RoleActivities',
+      'description' => 'RoleActivities (nl.pum.civireports)',
+      'class_name' => 'CRM_Civireports_Form_Report_RoleActivities',
+      'report_url' => 'nl.pum.civireports/roleactivities',
+      'component' => '',
+    ),
+  ),
+);
